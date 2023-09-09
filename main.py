0ba8b4c5a2ec82f641fe2e56058231d99ab41cfe
SKYFALL <arsyakost@gmail.com>
2023-09-09 18:36:26 +0500

    Version2

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import vk_api
+import vk_api #нужно скачать модуль pip install vk_api
 
 
+user_token = input('Введите ваш токен https://vkhost.github.io   ==>  ')
 
-#user_token = input('Введите ваш токен https://vkhost.github.io   ==>  ')
-
-session = vk_api.VkApi(token='vk1.a.QzRAo7KUj1qSWql8rmAuoDdCJ9jjEDvbkA0074NY0_p4_mpVIxSdnZLlrjDgl49MR5Nb-LdUQMY99wM_zS5g1msDggH9yTqYKRxk308Ew6x-tpwVBsRUKQFxLwCWcUpbM75QIOCFKawwJxi5kiFV25Fw44d9-E2I6NUqM2276jDCstSudzbBty5ZFBmlXNFUTwk-cCsxZuLCWeu9tEa2QQ')
-vk = session.get_api()
+session = vk_api.VkApi(token=user_token)
 
 
-user_id = session.method('user_id.get')
-
-print(user_id)
-
-def delete_wall(owner_id, post_id):
-    session.method('wall.delete', {'owner_id': owner_id, 'post_id':post_id})
-    # vk.wall.delete(owner_id, post_id)
+def delete_wall(post_id):        #Удаление поста
+    session.method('wall.delete', { 'post_id':post_id})
 
 
 def delete_all_wall(count):
@@ -24,14 +17,10 @@ def delete_all_wall(count):
         posts_list.append(post['id'])
     
     for post in posts_list:
-        delete_wall(310513537, post)
+        delete_wall(post)
         print(post)
 
-#delete_all_wall(100)
-# delete_wall(310513537, 1)
-
-
-#vk1.a.QzRAo7KUj1qSWql8rmAuoDdCJ9jjEDvbkA0074NY0_p4_mpVIxSdnZLlrjDgl49MR5Nb-LdUQMY99wM_zS5g1msDggH9yTqYKRxk308Ew6x-tpwVBsRUKQFxLwCWcUpbM75QIOCFKawwJxi5kiFV25Fw44d9-E2I6NUqM2276jDCstSudzbBty5ZFBmlXNFUTwk-cCsxZuLCWeu9tEa2QQ
+delete_all_wall(100) #Удалить сто постов 
 
 
 
